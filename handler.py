c4381af6a3fd253b9c0f8facf3f52506a2cd649e
Bruno Di Franco Albuquerque <brunodifranco99@gmail.com>
2022-10-08 17:34:30 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ from rossmann.rossmann import Rossmann
 import os
 
 # Loading model
-model = pickle.load(open('/model/model_rossmann.pkl', 'rb'))
+model = pickle.load(open('model/model_rossmann.pkl', 'rb'))
 
 # Initialize API
 app = Flask(__name__)
